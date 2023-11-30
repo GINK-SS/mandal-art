c4094d2f82aaa84cca7a7ea2b1e56b0eeb9a57f9
Sangmin Lee <gink.ss00@gmail.com>
2023-11-30 23:53:29 +0900

    Refactor: 테이블 클래스 스타일 묶기
    - 똑같은 긴 스타일 코드가 반복되어 스타일 변수로 묶고 한 곳에서 변경하도록 함

--- src/app/table.tsx
+++ src/app/table.tsx
@@ -6,39 +6,43 @@ type TableProps = {
 };
 
 export default function Table({ elements }: TableProps) {
+  const style = {
+    trStyle: 'flex',
+    tdStyle: 'flex items-center justify-center p-0 border',
+  };
   return (
     <table className="border-0 border-gray-500">
       <tbody>
-        <tr className="flex">
-          <td className="flex items-center justify-center p-0 border">
+        <tr className={style.trStyle}>
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[0]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[1]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[2]?.placeholder} />
           </td>
         </tr>
-        <tr className="flex">
-          <td className="flex items-center justify-center p-0 border">
+        <tr className={style.trStyle}>
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[3]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[4]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[5]?.placeholder} />
           </td>
         </tr>
-        <tr className="flex">
-          <td className="flex items-center justify-center p-0 border">
+        <tr className={style.trStyle}>
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[6]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[7]?.placeholder} />
           </td>
-          <td className="flex items-center justify-center p-0 border">
+          <td className={style.tdStyle}>
             <Textarea placeholder={elements[8]?.placeholder} />
           </td>
         </tr>
