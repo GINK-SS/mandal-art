b57111090b8c4ef61455ba6240171cf5893b3005
Sangmin Lee <gink.ss00@gmail.com>
2023-11-30 18:21:51 +0900

    Feat: 추가 상단 제목
    - 상단 제목 추가
    - 상단 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Table from './table';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="text-center cursor-default">
+        <h1 className="text-5xl font-extrabold">만다라트 계획표</h1>
+        <p>목표를 계획하거나 아이디어를 구체화 시킬 때 아주 유용하게 활용할 수 있습니다.</p>
+      </div>
+    </>
+  );
 }
