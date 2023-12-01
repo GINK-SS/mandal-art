a1fbaadfa688438f34364806c27b2ac47dd1d8b1
Sangmin Lee <gink.ss00@gmail.com>
2023-12-01 20:38:11 +0900

    Style: 테이블 태그 포맷 변경
    - 9개의 <td /> 태그를 map 메소드를 이용해서 간략하게 표현함

--- src/app/table.tsx
+++ src/app/table.tsx
@@ -7,91 +7,26 @@ type TableProps = {
 };
 
 export default function Table({ tId, elements }: TableProps) {
-  const style = {
-    trStyle: 'flex',
-    tdStyle: 'flex items-center justify-center p-0 border',
-  };
   return (
     <table className="border-0 border-gray-500">
       <tbody>
-        <tr className={style.trStyle}>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[0].id}
-              content={elements[0].content}
-              placeholder={elements[0]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[1].id}
-              content={elements[1].content}
-              placeholder={elements[1]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[2].id}
-              content={elements[2].content}
-              placeholder={elements[2]?.placeholder}
-            />
-          </td>
-        </tr>
-        <tr className={style.trStyle}>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[3].id}
-              content={elements[3].content}
-              placeholder={elements[3]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[4].id}
-              content={elements[4].content}
-              placeholder={elements[4]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[5].id}
-              content={elements[5].content}
-              placeholder={elements[5]?.placeholder}
-            />
-          </td>
-        </tr>
-        <tr className={style.trStyle}>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[6].id}
-              content={elements[6].content}
-              placeholder={elements[6]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[7].id}
-              content={elements[7].content}
-              placeholder={elements[7]?.placeholder}
-            />
-          </td>
-          <td className={style.tdStyle}>
-            <Textarea
-              tId={tId}
-              id={elements[8].id}
-              content={elements[8].content}
-              placeholder={elements[8]?.placeholder}
-            />
-          </td>
-        </tr>
+        {elements.map(
+          (_, index) =>
+            index % 3 === 0 && (
+              <tr key={index} className="flex">
+                {elements.slice(index, index + 3).map((element, index) => (
+                  <td key={index} className="flex items-center justify-center p-0 border">
+                    <Textarea
+                      tId={tId}
+                      id={element.id}
+                      content={element.content}
+                      placeholder={element?.placeholder}
+                    />
+                  </td>
+                ))}
+              </tr>
+            )
+        )}
       </tbody>
     </table>
   );
