b9395214bec221657bfd1df0da2f586ddf336f1d
Sangmin Lee <gink.ss00@gmail.com>
2023-12-05 00:55:21 +0900

    Feat: 테이블 제목 생성
    - 제목의 내용에 맞춰 자동 높이 조절되도록 함

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,15 +3,34 @@ import { State } from '@/redux/store';
 import Table from './table';
 import TableWrapper from './tableWrapper';
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 
 export default function Create() {
   const project = useSelector((state: State) => state.tableReducer);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleResizeHeight = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+    }
+  };
 
   return (
+    <div>
+      <textarea
+        ref={textareaRef}
+        rows={1}
+        className="w-full px-4 pb-2 mb-2 text-3xl font-medium border-b border-gray-300 outline-none resize-none focus:border-indigo-500"
+        placeholder="제목"
+        spellCheck={false}
+        onChange={handleResizeHeight}
+      />
       <TableWrapper>
         {project.tables.map((table, tIdx) => (
           <Table key={tIdx} tIdx={tIdx} isActive={table.isActive} elements={table.elements} />
         ))}
       </TableWrapper>
+    </div>
   );
 }
